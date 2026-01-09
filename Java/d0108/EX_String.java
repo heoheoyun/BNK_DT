b39f871c1b9ea5cc69bff6850157817e421f206d
heoheoyun <105352128+heoheoyun@users.noreply.github.com>
2026-01-09 09:45:53 +0900

    Update EX_String.java

--- Java/d0108/EX_String.java
+++ Java/d0108/EX_String.java
@@ -46,7 +46,45 @@ public class EX_String {
 		
 		//10. 문자열 비교 : equals()
 		String s1 = "abc";
-		String s2 = "abcd";
+		String s2 = "abc";
 		System.out.println(s1.equals(s2));
+		
+		String s3 = new String("abc");
+		System.out.println(s1 == s3);
+		System.out.println(s1.equals(s3));
+		
+		//11. 접두사/접미사 확인 : startWith()/endsWith()
+		System.out.println("14. startWitch(\"Hello\"): " + str1.startsWith("Hello"));
+		System.out.println("15. endsWitch(\"!\"): " + str1.endsWith("!"));
+		
+		//12. 부분 문자열 포함 : contains()
+		System.out.println("16. contains(\"World\") : "+ str1.contains("World"));
+		
+		//13. 문자열 비교(대소문자 구분없이)	: equalsIgnoreCase()
+		String s4 = "hello, world!";
+		System.out.println("17. equals(s4) : " + str1.equals(s4));
+		System.out.println("18. equalsIgnoreCase(s4) : " + str1.equalsIgnoreCase(s4));
+		
+		//14. 문자열의 사전적 순서 비교 : compareTo()
+		String s5 = "abc";
+		String s6 = "bcd";
+		System.out.println("19. \"abc\".compareTo(\"bc\") : " + s5.compareTo(s6));
+		System.out.println("19.5. \"abc\".compareTo(\"abc\") : " + s5.compareTo(s5));
+		System.out.println("20. \"bc\".compareTo(\"abc\") : " + s6.compareTo(s5));
+		
+		//15. 문자열 연결 : concat()
+		System.out.println("21. concat(\" Welcome\") : " + s5.concat(" Welcome"));
+		
+		//16. 다른 타입을 문자열로 변환 : valueOf()
+		int num = 123;
+		double b = 12.34;
+		System.out.println("22. valueOf(int) : " + String.valueOf(num));
+		System.out.println("23. valueOf(double) : "+ String.valueOf(b));
+		
+		//17. 형식화된 문자열 : format()
+		String formatted = String.format("이름: %s, 나이 %d, 키: %.1f", "홍길동", 25, 175.37);
+		System.out.println("24. format() : "+formatted);
+		
+		
 	}
 }
